49edbdea251c5c0828a2706a0e4a008578f451a9
Jordan Lee Osier <jordan.osier@skillmaple.com>
2023-03-17 08:41:24 -0400

    initialize sveltekit project

---
+++ src/routes/residential/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+
+<style>
+
+</style>
+
+
+<div class="navbar">
+    <div class="navbar-title"><a href="/">Atalon Green Buy Button Demo</a></div>
+    <ul class="navbar-links">
+        <li><a href="/residential">Residential</a></li>
+        <li><a href="/commercial-lite">Commercial Lite</a></li>
+        <li><a href="/commercial-heavy">Heavy Commercial</a></li>
+    </ul>
+</div>
+<h1>Residential Filter</h1>
