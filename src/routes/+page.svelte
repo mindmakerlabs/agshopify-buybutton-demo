1f45419d502707d696e628492d78b3b9329c72bc
Jordan Lee Osier <jordan.osier@skillmaple.com>
2023-03-17 11:14:52 -0400

    Add buy button for collection with attribution attempt

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,147 @@
-<script>
-</script>
-
-
-<style>
-
-</style>
-
 
 <div class="navbar">
-    <div class="navbar-title"><a href="/">Atalon Green Buy Button Demo</a></div>
-    <ul class="navbar-links">
-        <li><a href="/residential">Residential</a></li>
-        <li><a href="/commercial-lite">Commercial Lite</a></li>
-        <li><a href="/commercial-heavy">Heavy Commercial</a></li>
-    </ul>
+	<div class="navbar-title"><a href="/">Atalon Green Buy Button Demo</a></div>
+	<ul class="navbar-links">
+		<li><a href="/residential">Residential</a></li>
+		<li><a href="/commercial-lite">Commercial Lite</a></li>
+		<li><a href="/commercial-heavy">Heavy Commercial</a></li>
+	</ul>
 </div>
 <h1>Electrical Filters</h1>
+
+<div id="collection-component-1679065449980" />
+
+<script>
+	// @ts-nocheck
+
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		/*<![CDATA[*/
+		/*<![CDATA[*/
+		(function () {
+			var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
+			if (window.ShopifyBuy) {
+				if (window.ShopifyBuy.UI) {
+					ShopifyBuyInit();
+				} else {
+					loadScript();
+				}
+			} else {
+				loadScript();
+			}
+			function loadScript() {
+				var script = document.createElement('script');
+				script.async = true;
+				script.src = scriptURL;
+				(
+					document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]
+				).appendChild(script);
+				script.onload = ShopifyBuyInit;
+			}
+			function ShopifyBuyInit() {
+				var client = ShopifyBuy.buildClient({
+					domain: 'shop.atalongreen.com',
+					storefrontAccessToken: '110390f28df70a4e52acdf75d843833d'
+				});
+				ShopifyBuy.UI.onReady(client).then(function (ui) {
+					ui.createComponent('collection', {
+						id: '294547652788',
+						node: document.getElementById('collection-component-1679065449980'),
+						moneyFormat: '%24%7B%7Bamount%7D%7D',
+						options: {
+							product: {
+								styles: {
+									product: {
+										'@media (min-width: 601px)': {
+											'max-width': 'calc(25% - 20px)',
+											'margin-left': '20px',
+											'margin-bottom': '50px',
+											width: 'calc(25% - 20px)'
+										},
+										img: {
+											height: 'calc(100% - 15px)',
+											position: 'absolute',
+											left: '0',
+											right: '0',
+											top: '0'
+										},
+										imgWrapper: {
+											'padding-top': 'calc(75% + 15px)',
+											position: 'relative',
+											height: '0'
+										}
+									}
+								},
+								buttonDestination: 'modal',
+								contents: {
+									options: false
+								},
+								text: {
+									button: 'View product'
+								}
+							},
+							productSet: {
+								styles: {
+									products: {
+										'@media (min-width: 601px)': {
+											'margin-left': '-20px'
+										}
+									}
+								}
+							},
+							modalProduct: {
+								contents: {
+									img: false,
+									imgWithCarousel: true,
+									button: false,
+									buttonWithQuantity: true
+								},
+								styles: {
+									product: {
+										'@media (min-width: 601px)': {
+											'max-width': '100%',
+											'margin-left': '0px',
+											'margin-bottom': '0px'
+										}
+									}
+								},
+								text: {
+									button: 'Add to cart'
+								}
+							},
+							option: {},
+							cart: {
+								text: {
+									total: 'Subtotal',
+									button: 'Checkout'
+								}
+							},
+							toggle: {}
+						},
+						on: {
+							beforeInit: function (component) {
+								// Override the addItem function to include customAttributes
+                console.log("running before init")
+								var originalAddItem = component.props.cart.addItem;
+								component.props.cart.addItem = function (item) {
+									// Add customAttributes to the line item
+									item.customAttributes = [
+										{
+											key: 'Affiliate ID',
+											value: 'AffiliateID123'
+										}
+									];
+                  console.log("adding attribution info")
+                  console.log(item.customAttributes)
+									return originalAddItem.call(component.props.cart, item);
+								};
+							}
+						}
+					});
+				});
+			}
+		})();
+		/*]]>*/
+	});
+</script>
